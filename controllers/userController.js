0f6aa25253d7ca181db6e6b5eebfd48bff00049c
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2024-01-22 16:50:32 -0600

    user endpoints created

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User')
+
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+        const userUpdate = await User.findOneAndUpdate({ _id: req.params.id}, req.body);
+        res.status(200).json(userUpdate);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+  
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
